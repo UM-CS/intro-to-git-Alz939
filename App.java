b436385494986d8a4b8c90655e3cb3fcead441eb
Eddy <157830474+Alz939@users.noreply.github.com>
2024-10-02 15:00:35 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+public class App {
+
+     static String[][][] Faces = new String[6][3][3];
+    public static void main(String[] args){
+        for (int i = 0; i < Faces.length; i++) {
+            for (int j = 0; j < Faces[i].length; j++) {
+                for (int k = 0; k < Faces[i][j].length; k++){
+                    System.out.print(Faces[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
